dbbf926f5dc81475cbd074adf20363877fb2bbe4
LuisPiriz <manupirizcorrea@gmail.com>
2023-01-05 21:05:55 +0000

    v2 Iplementado el generador de cartas a traves del input

--- src/app.js
+++ src/app.js
@@ -2,33 +2,10 @@
 import "bootstrap";
 import "./style.css";
 
-let button = document.querySelector("#button");
-button.addEventListener(
-  "click",
-  (window.onload = function randomCard() {
-    //write your code here
+window.onload = function() {
+  //write your code here
+  function randomCard() {
     const palos = ["♦", "♥", "♠", "♣"];
-
-    let indexPalo = Math.floor(Math.random() * palos.length);
-
-    let top = document.querySelector(".top");
-    top.innerHTML = palos[indexPalo];
-    // top.style.color = palos[indexPalo];
-
-    let bot = document.querySelector(".bot");
-    bot.innerHTML = palos[indexPalo];
-
-    if (palos[indexPalo] == "♥" || palos[indexPalo] == "♦") {
-      top.style.color = "red";
-    } else {
-      top.style.color = "black";
-    }
-    if (palos[indexPalo] == "♥" || palos[indexPalo] == "♦") {
-      bot.style.color = "red";
-    } else {
-      bot.style.color = "black";
-    }
-
     const numeros = [
       "2",
       "3",
@@ -38,26 +15,79 @@ button.addEventListener(
       "7",
       "8",
       "9",
-      "10",
       "J",
       "K",
       "Q",
       "A"
     ];
 
+    let indexPalo = Math.floor(Math.random() * palos.length);
     let indexNumeros = Math.floor(Math.random() * numeros.length);
+    let palo = palos[indexPalo]; //Aquí obtengo el palo
+    let numero = numeros[indexNumeros];
+    console.log(palo);
+    console.log(numero);
+    let carta = [palo, numero];
+    console.log(carta);
 
-    let numbers = document.querySelector(".numero");
-    numbers.innerHTML = numeros[indexNumeros];
-
-    for (let i = 1; i <= 3; i++) {
-      document.body.innerHTML += `<div class="card m-auto">
-      <div class="card-body">
+    if (palos[indexPalo] == "♥" || palos[indexPalo] == "♦") {
+      document.getElementById("cards").innerHTML += `<div class="card d-flex">
+      <div class="card-body m-2">
+        <div class="top text-danger">${palos[indexPalo]}</div>
+        <div class="numero text-danger">${numeros[indexNumeros]}</div>
+        <div class="bot text-danger">${palos[indexPalo]}</div>
+      </div>
+    </div>`;
+    } else {
+      document.getElementById("cards").innerHTML += `<div class="card d-flex">
+      <div class="card-body m-2">
         <div class="top">${palos[indexPalo]}</div>
         <div class="numero">${numeros[indexNumeros]}</div>
         <div class="bot">${palos[indexPalo]}</div>
       </div>
     </div>`;
     }
-  })
-);
+    return carta;
+  }
+
+  let todascartas = [];
+
+  function variasCartas(n) {
+    document.getElementById("cards").innerHTML = "";
+    for (let i = 1; i <= input.value; i++) {
+      let newCard = randomCard();
+      todascartas.push(newCard);
+    }
+  }
+
+  document.getElementById("button").addEventListener("click", () => {
+    let tomavalor = document.getElementById("input");
+    variasCartas(tomavalor.value);
+  });
+
+  let sort = document.getElementById("button2");
+  // sort.addEventListener("click", bubbleSort);
+  let bubble = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // document.getElementById("button2").addEventListener("click", () => {
+  //   let tomavalor2 = bubble;
+  //   bubbleSort(tomavalor2);
+  // });
+
+  const bubbleSort = arr => {
+    let wall = arr.length - 1; //iniciamos el wall o muro al final del array
+    while (wall > 0) {
+      let index = 0;
+      while (index < wall) {
+        //comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
+        if (arr[index] > arr[index + 1]) {
+          let aux = arr[index];
+          arr[index] = arr[index + 1];
+          arr[index + 1] = aux;
+        }
+        index++;
+      }
+      wall--; //disminuir la pared para optimizar
+    }
+    return arr;
+  };
+};
