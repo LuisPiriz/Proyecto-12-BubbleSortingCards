a69dea64eb0b41e7f6fb9a6b5e4db9d20e492d46
LuisPiriz <manupirizcorrea@gmail.com>
2023-01-10 21:40:11 +0000

    FINALLY

--- src/app.js
+++ src/app.js
@@ -4,9 +4,12 @@ import "./style.css";
 
 window.onload = function() {
   //write your code here
+  let todascartas = [];
+
   function randomCard() {
     const palos = ["♦", "♥", "♠", "♣"];
     const numeros = [
+      "A",
       "2",
       "3",
       "4",
@@ -17,25 +20,26 @@ window.onload = function() {
       "9",
       "J",
       "K",
-      "Q",
-      "A"
+      "Q"
     ];
+    const valores = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
     let indexPalo = Math.floor(Math.random() * palos.length);
     let indexNumeros = Math.floor(Math.random() * numeros.length);
     let palo = palos[indexPalo]; //Aquí obtengo el palo
     let numero = numeros[indexNumeros];
-    console.log(palo);
-    console.log(numero);
-    let carta = [palo, numero];
-    console.log(carta);
+    let valor = valores[indexNumeros];
+    // console.log(palo);
+    // console.log(numero);
+    let carta = [palo, numero, valor];
+    // console.log(carta);
 
     if (palos[indexPalo] == "♥" || palos[indexPalo] == "♦") {
       document.getElementById("cards").innerHTML += `<div class="card d-flex">
       <div class="card-body m-2">
-        <div class="top text-danger">${palos[indexPalo]}</div>
-        <div class="numero text-danger">${numeros[indexNumeros]}</div>
-        <div class="bot text-danger">${palos[indexPalo]}</div>
+        <div class="top text-danger" id="top">${palos[indexPalo]}</div>
+        <div class="numero text-danger" id="numero">${numeros[indexNumeros]}</div>
+        <div class="bot text-danger" id="bot">${palos[indexPalo]}</div>
       </div>
     </div>`;
     } else {
@@ -50,44 +54,75 @@ window.onload = function() {
     return carta;
   }
 
-  let todascartas = [];
-
-  function variasCartas(n) {
+  function variasCartas() {
     document.getElementById("cards").innerHTML = "";
     for (let i = 1; i <= input.value; i++) {
       let newCard = randomCard();
       todascartas.push(newCard);
     }
   }
-
+  // console.log(todascartas);
+  // Boton para mostrar las cartas con el input
   document.getElementById("button").addEventListener("click", () => {
     let tomavalor = document.getElementById("input");
     variasCartas(tomavalor.value);
   });
 
+  // Boton para ordenar
   let sort = document.getElementById("button2");
-  // sort.addEventListener("click", bubbleSort);
-  let bubble = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // document.getElementById("button2").addEventListener("click", () => {
-  //   let tomavalor2 = bubble;
-  //   bubbleSort(tomavalor2);
-  // });
+  sort.addEventListener("click", function() {
+    bubbleSort([todascartas]);
+  });
 
-  const bubbleSort = arr => {
-    let wall = arr.length - 1; //iniciamos el wall o muro al final del array
+  const bubbleSort = todascartas => {
+    console.log("A ver: " + todascartas);
+
+    todascartas = todascartas[0];
+    let wall = todascartas.length - 1;
+    document.getElementById("cards").innerHTML = ``; //iniciamos el wall o muro al final del todascartasay
+
+    console.log("A ver otra cosa: " + wall);
     while (wall > 0) {
       let index = 0;
       while (index < wall) {
+        let cosa1 = todascartas[index];
+        let cosa2 = todascartas[index + 1];
+        console.log(cosa1);
         //comparar las posiciones adyacentes, si la correcta es más grande, tenemos que intercambiar
-        if (arr[index] > arr[index + 1]) {
-          let aux = arr[index];
-          arr[index] = arr[index + 1];
-          arr[index + 1] = aux;
+        if (parseInt(cosa1[2]) > parseInt(cosa2[2])) {
+          let aux = todascartas[index];
+          todascartas[index] = todascartas[index + 1];
+          todascartas[index + 1] = aux;
         }
+        console.log("A ver si ordena: " + todascartas);
         index++;
       }
       wall--; //disminuir la pared para optimizar
     }
-    return arr;
+    // return todascartas;
+
+    while (todascartas.length > 0) {
+      let malditasCartas = todascartas[0];
+      todascartas.shift();
+      let paloCarta = malditasCartas[0];
+
+      if (paloCarta[0] == "♥" || paloCarta[0] == "♦") {
+        document.getElementById("cards").innerHTML += `<div class="card d-flex">
+          <div class="card-body m-2">
+            <div class="top text-danger">${malditasCartas[0]}</div>
+            <div class="numero text-danger">${malditasCartas[1]}</div>
+            <div class="bot text-danger">${malditasCartas[0]}</div>
+          </div>
+        </div>`;
+      } else {
+        document.getElementById("cards").innerHTML += `<div class="card d-flex">
+          <div class="card-body m-2">
+            <div class="top">${malditasCartas[0]}</div>
+            <div class="numero">${malditasCartas[1]}</div>
+            <div class="bot">${malditasCartas[0]}</div>
+          </div>
+        </div>`;
+      }
+    }
   };
 };
